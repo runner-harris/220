f5de740531722d0e6bdd06bb44aceba7144336cb
Walker Harris <harriswh@g.cofc.edu>
2021-09-01 12:25:13 -0400

    completed hw1, walker harris

--- assignments/hw1/hello.py
+++ assignments/hw1/hello.py
@@ -1,4 +1,13 @@
-"""Homework 1 module."""
+"""
+Name: Walker Harris
+hello.py
+
+Problem: Write a function that prints "hello, world!" to the screen.
+
+Certification of Authenticity:
+I hereby certify that all work on this assignment is entirely my own.
+
+"""
 def main():
     print("hello, world!")
 
